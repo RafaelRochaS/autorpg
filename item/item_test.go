c153a9d978d7664a587fc8cfeabbc170645fbf70
rafael <rafaelrochasouza@outlook.com.br>
2022-09-24 22:44:57 -0300

    even more tests!

--- item/item_test.go
+++ item/item_test.go
@@ -3,8 +3,16 @@ package item
 import "testing"
 
 func TestItemString(t *testing.T) {
-	t.Run("Check item type string is not empty", func(t *testing.T) {
+	t.Run("Check item type - weapon - string is not empty", func(t *testing.T) {
 		itemType := WEAPON
+
+		if itemType.String() == "" {
+			t.Error("Failed to print item specs")
+		}
+	})
+
+	t.Run("Check item type - armor - string is not empty", func(t *testing.T) {
+		itemType := ARMOR
 
 		if itemType.String() == "" {
 			t.Error("Failed to print item specs")
