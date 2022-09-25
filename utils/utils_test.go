ef9b62db40de1c1164cc66374eb9377d7ff8163b
rafael <rafaelrochasouza@outlook.com.br>
2022-09-25 20:03:04 -0300

    tests: finish utils tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -20,14 +20,30 @@ func TestRandoms(t *testing.T) {
 	t.Run("Assert random number returns a number between the range", func(t *testing.T) {
 		randNumber := GetRandomNumberInRange(10, 20)
 		if randNumber > 20 || randNumber < 10 {
-			t.Errorf("TestRandoms :: random number : received  number not in range: %d", randNumber)
+			t.Errorf("TestRandoms - random number :: received  number not in range: %d", randNumber)
 		}
 	})
 
 	t.Run("Assert random float returns a float between the range", func(t *testing.T) {
 		randFloat := GetRandomFloatInRange(10, 20)
 		if randFloat > 20 || randFloat < 10 {
-			t.Errorf("TestRandoms :: random float : received  float not in range: %f", randFloat)
+			t.Errorf("TestRandoms - random float :: received  float not in range: %f", randFloat)
+		}
+	})
+
+	t.Run("Negative mins and maxs return 0 - int", func(t *testing.T) {
+		shouldBeZero := GetRandomNumberInRange(-1, -1)
+
+		if shouldBeZero != 0 {
+			t.Errorf("TestRandoms - negative mins and maxes :: did not return 0, got %d", shouldBeZero)
+		}
+	})
+
+	t.Run("Negative mins and maxs return 0 - float", func(t *testing.T) {
+		shouldBeZero := GetRandomFloatInRange(-1, -1)
+
+		if shouldBeZero != 0 {
+			t.Errorf("TestRandoms - negative mins and maxes :: did not return 0, got %f", shouldBeZero)
 		}
 	})
 }
