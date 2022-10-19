fed8452a0209d1c52caa4da6d5afbcb79e4069fd
rafael <rafaelrochasouza@outlook.com.br>
2022-10-18 23:04:13 -0300

    add: combat testing

--- combat/combat_test.go
+++ combat/combat_test.go
@@ -15,6 +15,7 @@ import (
 func TestCombat(t *testing.T) {
 	t.Run("Test execution - player wins", func(t *testing.T) {
 		utils.DEBUG = "True"
+
 		strongPlayer := character.NewCharacter(os.Stdin)
 		strongPlayer.AttachWeapon(&item.WeaponImpl{
 			DamageType:  item.NORMAL,
@@ -25,8 +26,25 @@ func TestCombat(t *testing.T) {
 			Weight:  1,
 			Defense: 10,
 		})
+		currentXp := strongPlayer.GetCurrentXp()
+
 		weakEnemy := enemy.NewEnemy(1)
+		givenXp := weakEnemy.GetXpGiven()
 
 		assert.NotPanics(t, func() { NewCombat().InitiateCombat(strongPlayer, weakEnemy) })
+		if strongPlayer.GetCurrentXp() != (currentXp + givenXp) {
+			t.Errorf("Player Wins :: xp error, wanted %d, got %d", (currentXp + givenXp), strongPlayer.GetCurrentXp())
+		}
+	})
+
+	t.Run("Test execution - enemy wins", func(t *testing.T) {
+		utils.DEBUG = "True"
+
+		weakPlayer := character.NewCharacter(os.Stdin)
+		weakPlayer.Create()
+
+		strongEnemy := enemy.NewEnemy(300)
+
+		assert.NotPanics(t, func() { NewCombat().InitiateCombat(weakPlayer, strongEnemy) })
 	})
 }
